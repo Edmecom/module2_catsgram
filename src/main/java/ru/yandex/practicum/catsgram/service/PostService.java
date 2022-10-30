f958b6817a1643ecc09682241fd52253d85959e5
Edmecom <edmasl@yandex.ru>
2022-10-30 15:34:26 +0300

    fix: тема Параметры запроса и формат ответа, урок 3

--- src/main/java/ru/yandex/practicum/catsgram/service/PostService.java
+++ src/main/java/ru/yandex/practicum/catsgram/service/PostService.java
@@ -1,32 +1,40 @@
 package ru.yandex.practicum.catsgram.service;
 
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import ru.yandex.practicum.catsgram.controller.PostController;
+import ru.yandex.practicum.catsgram.exception.PostNotFoundException;
 import ru.yandex.practicum.catsgram.exception.UserNotFoundException;
 import ru.yandex.practicum.catsgram.model.Post;
 import ru.yandex.practicum.catsgram.model.User;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class PostService {
     private final UserService userService;
     private final List<Post> posts = new ArrayList<>();
-    private static final Logger log = LoggerFactory.getLogger(PostController.class);
+    private static Integer globalId = 0;
 
     @Autowired
     public PostService(UserService userService) {
         this.userService = userService;
     }
 
-    public List<Post> findAll() {
-        log.debug("Текущее количество постов: {}", posts.size());
-        return posts;
+    public List<Post> findAll(Integer size, Integer from, String sort) {
+        return posts.stream().sorted((p0, p1) -> {
+            int comp = p0.getCreationDate().compareTo(p1.getCreationDate()); //прямой порядок сортировки
+            if(sort.equals("desc")){
+                comp = -1 * comp; //обратный порядок сортировки
+            }
+            return comp;
+        }).skip(from).limit(size).collect(Collectors.toList());
+    }
+
+    private static Integer getNextId(){
+        return globalId++;
     }
 
     public Post create(Post post) {
@@ -36,8 +44,25 @@ public class PostService {
                     "Пользователь %s не найден",
                     post.getAuthor()));
         }
-        log.debug("Пост сохранен");
+        post.setId(getNextId());
         posts.add(post);
         return post;
+    }
+
+    public Post findPostById(Integer postId) {
+        return posts.stream()
+                .filter(p -> p.getId().equals(postId))
+                .findFirst()
+                .orElseThrow(() -> new PostNotFoundException(String.format("Пост № %d не найден", postId)));
+    }
+
+    public List<Post> findAllByUserEmail(String email, Integer size, String sort) {
+        return posts.stream().filter(p -> email.equals(p.getAuthor())).sorted((p0, p1) -> {
+            int comp = p0.getCreationDate().compareTo(p1.getCreationDate()); //прямой порядок сортировки
+            if(sort.equals("desc")){
+                comp = -1 * comp; //обратный порядок сортировки
+            }
+            return comp;
+        }).limit(size).collect(Collectors.toList());
     }
 }
