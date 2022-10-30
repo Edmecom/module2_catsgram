a09d45824a1ad17154996349b6ed6a6190cd450e
Edmecom <edmasl@yandex.ru>
2022-10-30 15:34:01 +0300

    fix: тема Параметры запроса и формат ответа, урок 2

--- src/main/java/ru/yandex/practicum/catsgram/controller/PostController.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/PostController.java
@@ -3,19 +3,16 @@ package ru.yandex.practicum.catsgram.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.catsgram.model.Post;
 import ru.yandex.practicum.catsgram.service.PostService;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class PostController {
-    private static final Logger log = LoggerFactory.getLogger(PostController.class);
     private final PostService postService;
     @Autowired
     public PostController(PostService postService) {
@@ -23,13 +20,30 @@ public class PostController {
     }
 
     @GetMapping("/posts")
-    public List<Post> findAll() {
-        return postService.findAll();
+    public List<Post> findAll(
+        @RequestParam(value = "page", defaultValue = "0", required = false) Integer page,
+        @RequestParam(value = "size", defaultValue = "10", required = false) Integer size,
+        @RequestParam(value = "sort", defaultValue = "desc", required = false) String sort) {
+
+
+            if(!(sort.equals("asc") || sort.equals("desc"))){
+                throw new IllegalArgumentException();
+            }
+            if(page < 0 || size <= 0){
+                throw new IllegalArgumentException();
+            }
+
+            Integer from = page * size;
+            return postService.findAll(size, from, sort);
     }
 
     @PostMapping(value = "/post")
     public Post create(@RequestBody Post post) {
-        log.debug("Пост сохранен");
         return postService.create(post);
+    }
+
+    @GetMapping("/posts/{postId}")
+    public Post findById(@PathVariable("postId") Integer postId) {
+        return postService.findPostById(postId);
     }
 }
--- src/main/java/ru/yandex/practicum/catsgram/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/UserController.java
@@ -1,17 +1,11 @@
 package ru.yandex.practicum.catsgram.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.catsgram.exception.InvalidEmailException;
-import ru.yandex.practicum.catsgram.exception.UserAlreadyExistException;
 import ru.yandex.practicum.catsgram.model.User;
 import ru.yandex.practicum.catsgram.service.UserService;
 
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/users")
